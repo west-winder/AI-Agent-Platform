be4163e8fcb719fd8d99051b67d9ddba441f7a75
Xu Qiu <2262888348@qq.com>
2026-09-11 15:54:15 +0800

    feat(database): migrate application database to PostgreSQL

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,10 +1,28 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.engine import URL
+
+load_dotenv()  # 加载 .env 文件中的环境变量 
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
 
 
-DATABASE_URL = "sqlite:///./test.db"
-
+DATABASE_URL = URL.create(
+    drivername="postgresql+psycopg",
+    username=db_user,
+    password=db_password,
+    host=db_host,
+    port=int(db_port),
+    database=db_name
+)
 
 engine = create_engine(
     DATABASE_URL
